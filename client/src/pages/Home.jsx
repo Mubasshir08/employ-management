16b95f7eba5a1d3aa44f627bfaa7994f7752b1b7
Mubasshir08 <msr4212019@gmail.com>
2024-11-05 23:23:21 +0600

    home page design complete

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <section>
+      <div className='text-center mt-5'>
+          <h3 className=''>Welcome !</h3>
+          <h3 className='text-2xl'>Fhamida Afrin Sneha</h3>
+      </div>
+      <div className='grid grid-cols-2 pr-2.5 py-10'>
+        <div className='bg-neutral-100 rounded-2xl py-3 px-3 mt-3 ml-2'>Activity <span className='text-red-400 ml-16'>Bad</span></div>
+        <div className='bg-neutral-100 rounded-2xl py-3 px-2 mt-3 ml-2'>Total Earning <span className='text-green-800 ml-1'>200 BDT</span></div>
+        <div className='bg-neutral-100 rounded-2xl py-3 px-3 mt-3 ml-2'>Total Leave <span className='text-red-400 ml-10'>10</span></div>
+        <div className='bg-neutral-100 rounded-2xl py-3 px-3 mt-3 ml-2'>Total Deduction <span className='text-red-400 ml-3'>4</span></div>
+      </div>
+    </section>
   )
 }
 
--- client/src/shared/Navbar.jsx
+++ client/src/shared/Navbar.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react'
 import Sidebar from './Sidebar'
+import Logo from '../assets/logo.png'
 
 function Navbar() {
   const [showSidebar, setShowSidebar] = useState(false);
   return (
     <> 
-    <div className='flex flex-col items-center justify-center min-h-16 py-2 md:hidden bg-red-400'>
+    <div className='flex flex-row items-center min-h-16 py-2 md:hidden bg-red-400'>
+        <div className='w-[70px] h-[50px] -mt-12'>
+          <img src={Logo} alt="" srcset="" />
+        </div>
         <Sidebar showSidebar = {showSidebar} setShowSidebar = {setShowSidebar}/>
     </div>
 
